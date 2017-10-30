e5e47d4c76f01631f6cc1a81c07fd456e54c56fe
Geoffrey Arnold <geoffrey.arnold@pittsburghpa.gov>
2017-10-30 11:36:59 -0400

    Sign Change

--- app.R
+++ app.R
@@ -756,26 +756,26 @@ server <- shinyServer(function(input, output, session) {
     return(si)
   })
   # Stop
-  stopSignsLoad <- reactive({
-    stops <- ckan("cbbb69f2-d622-4dce-9587-43965146e436")
-    stops_df <- SpatialPointsDataFrame(cbind(stops$longitude, stops$latitude), stops)
+  signsLoad <- reactive({
+    signs <- ckan("d078a6b5-83a3-4723-a3a9-5371cfe1cc0c")
+    signs_df <- SpatialPointsDataFrame(cbind(signs$longitude, signs$latitude), signs)
     
-    return(stops_df)
+    return(signs_df)
     })  
-  stopsInput <- reactive({
-    stops <- stopSignsLoad()
+  signsInput <- reactive({
+    signs <- signsignsLoad()
     
     # Operation Type Filter
     if (length(input$intersection_select) > 0) {
-      stops <- stops[stops@data$description %in% input$intersection_select,]
+      signs <- signs[signs@data$description %in% input$intersection_select,]
     }
 
     # Search Filter
     if (!is.null(input$search) & input$search != "") {
-      stops <- stops[apply(stops@data, 1, function(row){any(grepl(input$search, row, ignore.case = TRUE))}), ]
+      signs <- signs[apply(signs@data, 1, function(row){any(grepl(input$search, row, ignore.case = TRUE))}), ]
     }
     
-    return(stops)
+    return(signs)
   })
   # Load Crosswalks
   loadCwLoad <- reactive({
@@ -1707,14 +1707,14 @@ server <- shinyServer(function(input, output, session) {
                                                   "<br><b>Location:</b>", cw$street, "</font>"))
           )
         }
-        stops <- stopsInput()
-        if (nrow(stops) > 0) {
+        signs <- signsInput()
+        if (nrow(signs) > 0) {
           assetsCount <- assetsCount + 1
-          map <- addCircleMarkers(map, data=stops, color = "#e41a1c", fillColor = "#e41a1c", fillOpacity = .5, radius = 2,
-                                  popup = ~(paste0("<font color='black'><b>Sign Type:</b> ", stops$description, " (", stops$mutcd_code, ")",
-                                                  "<br><b>Location:</b> ", paste(stops$address_number, stops$street),
-                                                  "<br><b>Mounting Fixture:</b> ", stops$mounting_fixture,
-                                                  ifelse(is.na(stops$date_installed),"" , paste0("<br><b>Installed Date:</b> ", stops$date_installed))))
+          map <- addCircleMarkers(map, data=signs, color = "#e41a1c", fillColor = "#e41a1c", fillOpacity = .5, radius = 2,
+                                  popup = ~(paste0("<font color='black'><b>Sign Type:</b> ", signs$description, " (", signs$mutcd_code, ")",
+                                                  "<br><b>Location:</b> ", paste(signs$address_number, signs$street),
+                                                  "<br><b>Mounting Fixture:</b> ", signs$mounting_fixture,
+                                                  ifelse(is.na(signs$date_installed),"" , paste0("<br><b>Installed Date:</b> ", signs$date_installed))))
                                                   
           )
         }
